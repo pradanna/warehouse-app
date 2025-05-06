3413159d3666c3a7f355140973011cc8cea06bfa
bagusyanuar <bagus.yanuar613@gmail.com>
2025-05-06 14:25:17 +0700

    feat: refactor purchasing scheme

---
+++ database/migrations/2025_05_06_141700_purchase_payments.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_payments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('purchase_id');
+            $table->date('date');
+            $table->text('description');
+            $table->enum('payment_type', ['cash', 'digital'])->default('cash');
+            $table->string('evidence')->nullable();
+            $table->uuid('author_id');
+            $table->timestamps();
+            $table->foreign('purchase_id')->references('id')->on('purchases')->onDelete('cascade');
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_payments');
+    }
+};
