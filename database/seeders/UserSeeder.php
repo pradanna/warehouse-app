06ba1559f7db0b440fdfb6c72c09db355f05c2b2
owlprojection <bagus.yanuar613@gmail.com>
2025-07-08 13:56:01 +0700

    feat: a

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Exception;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Ramsey\Uuid\Uuid;
 use Illuminate\Support\Str;
 use Ramsey\Uuid\Guid\Guid;
@@ -18,21 +20,30 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $role = Role::firstOrCreate(
-            ['name' => 'superadmin', 'guard_name' => 'web'],
-            ['id' => Str::uuid()->toString()]
-        );
+        DB::beginTransaction();
+        try {
+            $roleId = "b572ea4e-a0b3-444a-96f0-e97077379175";
+            $role = Role::where('id', $roleId)->first();
+            if (!$role) {
+                Log::error("role not found...");
+                DB::rollBack();
+            }
 
-        $user = User::firstOrCreate(
-            ['username' => 'superadmin'],
-            [
-                'password' => bcrypt('@Superadmin123'),
-                'id' => Str::uuid()->toString(),
-            ]
-        );
+            $user = User::firstOrCreate(
+                ['username' => 'superadmin'],
+                [
+                    'password' => bcrypt('@Superadmin123'),
+                    'id' => Str::uuid()->toString(),
+                ]
+            );
 
-        if (!$user->hasRole('superadmin')) {
-            $user->assignRole($role);
+            if (!$user->hasRole($role)) {
+                $user->assignRole($role);
+            }
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error("error seed users" . $th->getMessage());
         }
     }
 }
