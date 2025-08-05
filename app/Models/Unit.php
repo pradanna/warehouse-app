bfde2f0eb1a87a7803ce729366ecadb902619537
owlprojection <bagus.yanuar613@gmail.com>
2025-08-05 21:09:48 +0700

    feat: add soft delete to master

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Unit extends Model
 {
     //
-    use HasFactory, Uuid;
+    use HasFactory, Uuid, SoftDeletes;
 
     protected $keyType = 'string';
     public $incrementing = false;
